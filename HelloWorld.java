11f5162bc3dc02b847e98fd290e7e2be05e1ecf3
William Cook <wbcook007@gmail.com>
2015-04-07 22:59:11 -0400

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Write a program that prints ‘Hello World’ to the screen.
+
+public class HelloWorld {
+  public static void main(String []args) {
+    System.out.println("Hello, World!");
+  }
+}
