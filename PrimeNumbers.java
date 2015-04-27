e0b91a8b209a6aebcc0f90d4d4a1c4a266896b1d
William Cook <wbcook007@gmail.com>
2015-04-26 21:04:43 -0400

    PrimeNumbers

---
+++ PrimeNumbers.java
@@ -0,0 +1,11 @@
+// Print all primes up to the largest you can represent in your language.
+
+public class PrimeNumbers {
+  public static void main(String []arges) {
+    int maxValue = 10;//Integer.MAX_VALUE;
+    for (int p = 1; p <= maxValue; p++) {
+      for (int m = 2; m < p; m++) {
+      }
+    }
+  }
+}
