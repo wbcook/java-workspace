20b04901051b437cf5f3b6ec072f91d8fd517f8c
William Cook <wbcook007@gmail.com>
2015-05-15 19:55:14 -0400

    FizzBuzzArray

---
+++ FizzBuzzArray.java
@@ -0,0 +1,41 @@
+// Print a list of numbers from 1 to 100.
+// Print FizzBuzz for numbers divisible by 3 and 5.
+// Print Fizz for numbers divisible by 3, and Buzz for numbers divisible by 5.
+// Print the other numbers as they would normally appear.
+import java.io.Console;
+import java.util.Arrays;
+
+public class FizzBuzzArray {
+  public static void main(String []args) {
+    String[] fizzBuzzArray = new String[100];
+
+    fizzBuzzArray = fizzBuzzArrayBuilder(fizzBuzzArray);
+
+    fizzBuzzArrayPrinter(fizzBuzzArray);
+  }
+
+  public static String[] fizzBuzzArrayBuilder(String[] fizzBuzzArray) {
+    int range = 100;
+    for (int i = 1; i <= range; i++) {
+      int n = i-1;
+      if (i % 15 == 0) {
+        fizzBuzzArray[n] = "FizzBuzz!";
+      } else if ( i % 3 == 0) {
+        fizzBuzzArray[n] = "Fizz";
+      } else if ( i % 5 == 0) {
+        fizzBuzzArray[n] = "Buzz";
+      } else {
+        fizzBuzzArray[n] = Integer.toString(i);
+      }
+    }
+    return fizzBuzzArray;
+  }
+
+  public static void fizzBuzzArrayPrinter(String[] fizzBuzzArray) {
+    Console cons = System.console();
+    int l = fizzBuzzArray.length;
+    for (int i = 0; i < l; i++) {
+      cons.printf("\n%s", fizzBuzzArray[i]);
+    }
+  }
+}
